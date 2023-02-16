46dd995f542c8b3b13f2f8e2d8cb193bfafab83c
Tomi <hungshou.cheng@iisigroup.com>
2023-02-16 13:45:01 +0800

    加入專案檔案。

---
+++ CiDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CiDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet(Name = "GetVersion")]
+        public string GetVersion()
+        {
+            return "0.1";
+        }
+    }
+}
